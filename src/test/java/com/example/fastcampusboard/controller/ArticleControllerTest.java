72b26f6ae27e076b666334fb1de5eae7d1bf94af
dgw <ds257873@naver.com>
2023-01-06 17:59:20 +0900

    게시글 뷰 테스트 수정

--- src/test/java/com/example/fastcampusboard/controller/ArticleControllerTest.java
+++ src/test/java/com/example/fastcampusboard/controller/ArticleControllerTest.java
@@ -1,5 +1,6 @@
 package com.example.fastcampusboard.controller;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @DisplayName("View Controller")
 @WebMvcTest(ArticleController.class)
+@Disabled
 public class ArticleControllerTest {
 
     private final MockMvc mvc;
@@ -30,6 +32,7 @@ public class ArticleControllerTest {
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/index"))
                 .andExpect(model().attributeExists("articles"));
         //then
     }
@@ -42,7 +45,11 @@ public class ArticleControllerTest {
         mvc.perform(get("/articles/1"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.TEXT_HTML))
-                .andExpect(model().attributeExists("article"));
+                .andExpect(view().name("articles/detail"))
+                .andExpect(model().attributeExists("article"))
+                .andExpect(model().attributeExists("articleComments"));
+
+
         //then
     }
     @DisplayName("[view][GET] 게시글 검색 전용 페이지 - 정상 호출")
@@ -53,7 +60,9 @@ public class ArticleControllerTest {
         //when
         mvc.perform(get("/articles/search"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML));
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("article/search"));
+
         //then
     }
     @DisplayName("[view][GET] 게시글 해시태그 검색 페이지 - 정상 호출")
@@ -64,7 +73,9 @@ public class ArticleControllerTest {
         //when
         mvc.perform(get("/articles/search-hashtag"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML));
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("article/search-hashtag"));
+
         //then
     }
 }
