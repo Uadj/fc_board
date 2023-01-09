701b2f9c18dd4d1032b169df28799daae263b90b
dgw <30551889+Uadj@users.noreply.github.com>
2023-01-09 22:02:44 +0900

    Revert "게시판 뷰 만들기"

--- src/main/java/com/example/fastcampusboard/FastcampusBoardApplication.java
+++ src/main/java/com/example/fastcampusboard/FastcampusBoardApplication.java
@@ -2,9 +2,7 @@ package com.example.fastcampusboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
-@ConfigurationPropertiesScan
 @SpringBootApplication
 public class FastcampusBoardApplication {
 
