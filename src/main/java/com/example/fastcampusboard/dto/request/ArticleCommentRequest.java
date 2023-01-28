aa549f7dedd3032b3d1a043cb470de7a4653b03e
dgw <30551889+Uadj@users.noreply.github.com>
2023-01-28 18:28:53 +0900

    댓글, 대댓글 쓰기 삭제 추가

---
+++ src/main/java/com/example/fastcampusboard/dto/request/ArticleCommentRequest.java
@@ -0,0 +1,29 @@
+package com.example.fastcampusboard.dto.request;
+
+import com.example.fastcampusboard.dto.ArticleCommentDto;
+import com.example.fastcampusboard.dto.UserAccountDto;
+
+public record ArticleCommentRequest(
+        Long articleId,
+        Long parentCommentId,
+        String content
+) {
+
+    public static ArticleCommentRequest of(Long articleId, String content) {
+        return ArticleCommentRequest.of(articleId, null, content);
+    }
+
+    public static ArticleCommentRequest of(Long articleId, Long parentCommentId, String content) {
+        return new ArticleCommentRequest(articleId, parentCommentId, content);
+    }
+
+    public ArticleCommentDto toDto(UserAccountDto userAccountDto) {
+        return ArticleCommentDto.of(
+                articleId,
+                userAccountDto,
+                parentCommentId,
+                content
+        );
+    }
+
+}
