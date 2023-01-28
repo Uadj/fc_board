2dcfff5950be07b78ac6422274189da461c8836a
dgw <30551889+Uadj@users.noreply.github.com>
2023-01-28 18:49:37 +0900

    해시태그 검색 기능 추가

---
+++ src/main/java/com/example/fastcampusboard/repository/HashtagRepositoryCustomImpl.java
@@ -0,0 +1,24 @@
+package com.example.fastcampusboard.repository;
+
+import com.example.fastcampusboard.domain.Hashtag;
+import com.example.fastcampusboard.domain.QHashtag;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+
+import java.util.List;
+
+public class HashtagRepositoryCustomImpl extends QuerydslRepositorySupport implements HashtagRepositoryCustom {
+
+    public HashtagRepositoryCustomImpl() {
+        super(Hashtag.class);
+    }
+
+    @Override
+    public List<String> findAllHashtagNames() {
+        QHashtag hashtag = QHashtag.hashtag;
+
+        return from(hashtag)
+                .select(hashtag.hashtagName)
+                .fetch();
+    }
+
+}
