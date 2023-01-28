e3c3544b97c2dfc4643891ecb4be37f6c7fb930d
dgw <30551889+Uadj@users.noreply.github.com>
2023-01-28 11:30:22 +0900

    글쓰기 계정 보안 관련 코드

--- src/main/java/com/example/fastcampusboard/config/SecurityConfig.java
+++ src/main/java/com/example/fastcampusboard/config/SecurityConfig.java
@@ -1,8 +1,12 @@
 package com.example.fastcampusboard.config;
 
+import com.example.fastcampusboard.dto.security.BoardPrincipal;
+import com.example.fastcampusboard.service.UserAccountService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,5 +17,12 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
                 .formLogin().and()
                 .build();
+    }
+    @Bean
+    public UserDetailsService userDetailsService(UserAccountService userAccountService) {
+        return username -> userAccountService
+                .searchUser(username)
+                .map(BoardPrincipal::from)
+                .orElseThrow(() -> new UsernameNotFoundException("유저를 찾을 수 없습니다 - username: " + username));
     }
 }
---
+++ src/main/java/com/example/fastcampusboard/service/UserAccountService.java
@@ -0,0 +1,31 @@
+package com.example.fastcampusboard.service;
+
+import com.example.fastcampusboard.domain.UserAccount;
+import com.example.fastcampusboard.dto.UserAccountDto;
+import com.example.fastcampusboard.repository.UserAccountRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class UserAccountService {
+
+    private final UserAccountRepository userAccountRepository;
+
+    @Transactional(readOnly = true)
+    public Optional<UserAccountDto> searchUser(String username) {
+        return userAccountRepository.findById(username)
+                .map(UserAccountDto::from);
+    }
+
+    public UserAccountDto saveUser(String username, String password, String email, String nickname, String memo) {
+        return UserAccountDto.from(
+                userAccountRepository.save(UserAccount.of(username, password, email, nickname, memo, username))
+        );
+    }
+
+}
