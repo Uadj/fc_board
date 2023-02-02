f9e8572599a20c3026f987d79c9496270aab05b3
dgw <30551889+Uadj@users.noreply.github.com>
2023-02-02 13:25:39 +0900

    Update SecurityConfig.java

--- src/main/java/com/example/fastcampusboard/config/SecurityConfig.java
+++ src/main/java/com/example/fastcampusboard/config/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
                         .mvcMatchers(
                                 HttpMethod.GET,
                                 "/",
+                                "/articles,
                                 "/articles/*"
                         ).permitAll()
                         .anyRequest().authenticated()
